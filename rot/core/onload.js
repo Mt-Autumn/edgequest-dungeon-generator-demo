72253fd3db259fd12994f5c26b60d577942201c9
Gray <graymury@gmail.com>
2017-01-18 08:38:42 -0500

    Add browser detection

--- rot/core/onload.js
+++ rot/core/onload.js
@@ -1,3 +1,20 @@
+// Detect browser
+navigator.sayswho = (function() {
+    var ua = navigator.userAgent, tem,
+    M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if( /trident/i.test(M[1]) ) {
+        tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+        return 'IE ' + (tem[1] || '');
+    }
+    if( M[1] === 'Chrome' ){
+        tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
+        if(tem != null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+    M = M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+    return M.join(' ');
+})();
+
 // Load up the game
 window.onload = function() {
 
@@ -7,6 +24,14 @@ window.onload = function() {
         alert("The rot.js library isn't supported by your browser.");
 
     } else {
+
+        // Check browser
+        if (navigator.sayswho.split(" ")[0] === 'Firefox') {
+          // Known issue with firefox
+          if (parseInt(navigator.sayswho.split(" ")[1]) < 32) {
+            alert("The game may not be supported by your browser.");
+          }
+        }
 
         // Initialize Canvas
         Canvas.init();
